8985bcabf6adb90b73364769d7f3c69dcc6a4792
BenoitChoffin <benoit.choffin@hotmail.fr>
2020-02-27 10:37:38 -0500

    Add slicing decomposition

--- eval_metrics.py
+++ eval_metrics.py
@@ -8,6 +8,7 @@ import json
 import re
 import os
 
+SENSITIVE_ATTR = "school_id"
 
 def avgstd(l):
     '''
@@ -34,7 +35,9 @@ if __name__ == '__main__':
     test = df.iloc[indices]
 
     predictions_per_user = defaultdict(lambda: defaultdict(list))
-    metrics = defaultdict(list)
+    predictions_per_sensitive_attr = defaultdict(lambda: defaultdict(list))
+    metrics_per_user = defaultdict(list)
+    metrics_per_sensitive_attr = defaultdict(list)
 
     r = re.compile(r'results-(.*).json')
     for filename in glob.glob('results*2020*'):
@@ -52,6 +55,10 @@ if __name__ == '__main__':
             predictions_per_user[user]['pred'].append(pred)
             predictions_per_user[user]['y'].append(true)
 
+        for attr, pred, true in zip(test[SENSITIVE_ATTR], y_pred, y):
+            predictions_per_sensitive_attr[attr]['pred'].append(pred)
+            predictions_per_sensitive_attr[attr]['y'].append(true)            
+
         users_ids = []
         ndcg_ = []
         for user in predictions_per_user:
@@ -59,23 +66,36 @@ if __name__ == '__main__':
             this_true = np.array(predictions_per_user[user]['y'])
             if len(this_pred) > 1:
                 users_ids.append(user)
-                metrics['ndcg'].append(ndcg_score([this_true], [this_pred]))
-                metrics['ndcg@10'].append(ndcg_score([this_true], [this_pred], k=10))
-                metrics['ndcg-'].append(ndcg_score([1 - this_true], [1 - this_pred]))
+                metrics_per_user['ndcg'].append(ndcg_score([this_true], [this_pred]))
+                metrics_per_user['ndcg@10'].append(ndcg_score([this_true], [this_pred], k=10))
+                metrics_per_user['ndcg-'].append(ndcg_score([1 - this_true], [1 - this_pred]))
                 ndcg_.append(ndcg_score([1 - this_true], [1 - this_pred]))
-                metrics['ndcg@10-'].append(ndcg_score([1 - this_true], [1 -this_pred], k=10))
+                metrics_per_user['ndcg@10-'].append(ndcg_score([1 - this_true], [1 -this_pred], k=10))
             if len(np.unique(this_true)) > 1:
-                metrics['auc'].append(roc_auc_score(this_true, this_pred))
+                metrics_per_user['auc'].append(roc_auc_score(this_true, this_pred))
+
+        for attr in predictions_per_sensitive_attr:
+            this_pred = np.array(predictions_per_sensitive_attr[attr]['pred'])
+            this_true = np.array(predictions_per_sensitive_attr[attr]['y'])
+            if len(this_pred) > 1:
+                metrics_per_sensitive_attr['ndcg'].append(ndcg_score([this_true], [this_pred]))
+                metrics_per_sensitive_attr['ndcg@10'].append(ndcg_score([this_true], [this_pred], k=10))
+                metrics_per_sensitive_attr['ndcg-'].append(ndcg_score([1 - this_true], [1 - this_pred]))
+                ndcg_.append(ndcg_score([1 - this_true], [1 - this_pred]))
+                metrics_per_sensitive_attr['ndcg@10-'].append(ndcg_score([1 - this_true], [1 -this_pred], k=10))
+            if len(np.unique(this_true)) > 1:
+                attr_ids.append(attr)
+                metrics_per_sensitive_attr['auc'].append(roc_auc_score(this_true, this_pred))
     
         print(len(y))
         print(y[:10], test[:10])
 
         print('overall auc', roc_auc_score(y, y_pred))
-        print('auc', avgstd(metrics['auc']))
-        print('ndcg', avgstd(metrics['ndcg']))
-        print('ndcg@10', avgstd(metrics['ndcg@10']))
-        print('ndcg-', avgstd(metrics['ndcg-']))
-        print('ndcg@10-', avgstd(metrics['ndcg@10-']))
+        print('auc', avgstd(metrics_per_user['auc']))
+        print('ndcg', avgstd(metrics_per_user['ndcg']))
+        print('ndcg@10', avgstd(metrics_per_user['ndcg@10']))
+        print('ndcg-', avgstd(metrics_per_user['ndcg-']))
+        print('ndcg@10-', avgstd(metrics_per_user['ndcg@10-']))
 
         # Display ids of the students that have the lowest/highest ndcg-
         print("Lowest NDCG- = {} on user {}".format(np.around(np.min(ndcg_),5),users_ids[np.argmin(ndcg_)]))
@@ -84,3 +104,10 @@ if __name__ == '__main__':
         print("Highest NDCG- = {} on user {}".format(np.around(np.max(ndcg_),5),users_ids[np.argmax(ndcg_)]))
         print(np.array(predictions_per_user[users_ids[np.argmax(ndcg_)]]['y']))
         print(np.array(predictions_per_user[users_ids[np.argmax(ndcg_)]]['pred']))
+
+        # Display ids of the subgroups (sensitive attribute) that have the lowest/highest AUC
+        print("Lowest AUC = {} on subgroup {}".format(np.around(np.min(metrics_per_sensitive_attr['auc']),5),
+                                                      attr_ids[np.argmin(metrics_per_sensitive_attr['auc'])]))
+        print("Highest AUC = {} on subgroup {}".format(np.around(np.max(metrics_per_sensitive_attr['auc']),5),
+                                                       attr_ids[np.argmax(metrics_per_sensitive_attr['auc'])]))
+
